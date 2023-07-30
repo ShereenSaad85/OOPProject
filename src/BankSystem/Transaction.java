4c477d8062f96f2abc768247e35a14ce426533b8
ShereenSaad85 <shereen.saad1421@gmail.com>
2023-07-30 17:48:50 +0300

    delete src

--- src/BankSystem/Transaction.java
+++
@@ -1,9 +0,0 @@
-package BankSystem;
-
-public abstract class Transaction {
-    public void performingTransaction(){
-        System.out.println("Your transaction process is done successfully");
-    }
-
-
-}
--- src/BankSystem/WithdrawTransaction.java
+++
@@ -1,7 +0,0 @@
-package BankSystem;
-
-public class WithdrawTransaction extends  Transaction{
-    public void withdrawPerformTransaction(){
-        System.out.println("withdraw Perform Transaction");
-    }
-}
